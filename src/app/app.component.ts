67391464a6769772843d68c8c9c93a4621e53e5e
Velasquez <cp.velasquez@fujitsu.com>
2020-12-03 15:11:09 +0800

    PWA for BSE Forum

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, NgZone } from "@angular/core";
 
-import { Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { LanguageService } from './providers/language.service';
+import { Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { LanguageService } from "./providers/language.service";
+import { Router } from "@angular/router";
+import { Plugins } from "@capacitor/core";
+const { App } = Plugins;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    public languageService: LanguageService
+    public languageService: LanguageService,
+    private router: Router,
+    private zone: NgZone
   ) {
-
     this.initializeApp();
   }
 
@@ -26,6 +30,18 @@ export class AppComponent {
       this.languageService.initialize();
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      this.deepLinking();
+    });
+  }
+
+  deepLinking() {
+    App.addListener("appUrlOpen", (data: any) => {
+      this.zone.run(() => {
+        const path = data.url.split(".com").pop();
+        if (path) {
+          this.router.navigate(path.split("/"));
+        }
+      });
     });
   }
 }
